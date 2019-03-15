5c32290287708a68c0ab4864ead297770cf8f222
Fabio Girlando <fabiogirlando@Fabios-MacBook-Pro.local>
2019-03-15 14:28:18 +0100

    Basic Player Controls
    Bird was given physics control. Jump button makes the Bird flap. Rotation according to velocity.

---
+++ Flappy Bird Copy/Assets/Script/FlapControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FlapControl : MonoBehaviour
+{
+    [Tooltip("The veritical force by which the player will be pushed.")]
+    public float force = 10f;
+
+    [Tooltip("The rotation multiplier, to make turning more accurate.")]
+
+    public float rotationMultiplier = 5f;
+
+    [Tooltip("The minimum angle for this bird to turn.")]
+    public float minAngle = -90f;
+
+    [Tooltip("The minimum angle for this bird to turn.")]
+    public float maxAngle = 45f;
+    // Store the RigidBody2d component in memory for easier access.
+    private Rigidbody2D _rigidbody;
+
+    // Loading all the reference when the script is Awake.
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Physics should be allocated into FixedUpdate.
+
+    private void FixedUpdate()
+    {
+        float angle = Mathf.Clamp(_rigidbody.velocity.y * rotationMultiplier, minAngle, maxAngle);
+        _rigidbody.MoveRotation(angle);
+    }
+
+    // Handle all input control on Update for snappier responses.
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Flap();
+        }
+    }
+
+    // Causes the player to "jump"
+    public void Flap()
+    {
+        // We cannot change the velocity directly, but we can replace it
+        // as a vector. First, we create a copy.
+        Vector2 v = _rigidbody.velocity;
+
+        // Change the speed on the Y axis only.
+        v.y = force;
+
+        // Write the vector back to the rigidbody.
+        _rigidbody.velocity = v;
+    }
+}
